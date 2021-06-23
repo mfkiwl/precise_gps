d1cd655ffad8440a823c58e5e36ec1055491f0cf
ylajaaski <juhoylajaaski@gmail.com>
2021-06-24 00:06:22 +0300

    refactor : redwine json, base for creating visuals from created pkl files

--- src/visuals/visuals.py
+++ src/visuals/visuals.py
@@ -20,7 +20,7 @@ class MidpointNormalize(colors.Normalize):
         x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]
         return np.ma.masked_array(np.interp(value, x, y), np.isnan(value))
 
-def show_kernel(kernel, title, cols, save, aspect = "minmax", show_nums = -1, save_fig = False):
+def show_kernel(kernel, title, cols, aspect = "minmax", show_nums = -1, savefig = None):
     """
     Visualizes the kernel with colors.
 
@@ -28,15 +28,14 @@ def show_kernel(kernel, title, cols, save, aspect = "minmax", show_nums = -1, sa
         kernel (numpy array) : D x D shaped numpy array or tensorflow tensor
         title (string)       : text for the title
         cols (list)          : D x 1 list for the covariates
-        save (string)        : path in which the figure is saved (without .pdf extension)
         aspect (string)      : "minmax" -> colors are scaled from minimum to maximum (center is not zero)
                                "<anything else>" -> centered to zero
         show_nums (int)      : -1 -> dont show numeric values of the kernel
                                 > -1 -> show values of the kernel
-        save_fig (bool)      : whether the figure is saved to the specified path 
+        save_fig (string)    : path in which the figure is saved
     
     Returns:
-        Shows the figure and saves it to the specified path if <save_fig == True>.
+        Shows the figure and saves it to the specified path if <savefig not None>.
     """
     maximum = max(abs(np.min(kernel)), np.max(kernel))
     fig = plt.figure(figsize = (6,6))
@@ -60,11 +59,11 @@ def show_kernel(kernel, title, cols, save, aspect = "minmax", show_nums = -1, sa
                               ha="center", va="center", color="black")
 
     plt.colorbar(im, cax=cax)
-    if save_fig:
-        plt.savefig("{}.pdf".format(save), bbox_inches='tight')
+    if savefig:
+        plt.savefig(savefig, bbox_inches='tight')
     plt.show()
 
-def show_kernels(kernels, titles, cols, save, aspect = "own", show_nums = -1, save_fig = False):
+def show_kernels(kernels, titles, cols, aspect = "own", show_nums = -1, savefig = None):
     """
     Visualizes a list of kernel with colors.
 
@@ -72,12 +71,11 @@ def show_kernels(kernels, titles, cols, save, aspect = "own", show_nums = -1, sa
         kernels (list of numpy arrays) : [D x D] list of numpy arrays or tensorflow tensors
         title (list)                   : list of the title texts
         cols (list)                    : D x 1 list for the covariates
-        save (string)                  : path in which the figure is saved (without .pdf extension)
         aspect (string)                : "own" -> centered to zero with respect to won values
                                          "<anything else>" -> centered to zero with respect to global values
         show_nums (int)                : -1 -> dont show numeric values of the kernel
                                          > -1 -> show values of the kernel
-        save_fig (bool)                : whether the figure is saved to the specified path 
+        savefig (string)               : path in which the figure is saved
     
     Returns:
         Shows the figure and saves it to the specified path if <save_fig == True>.
@@ -110,18 +108,35 @@ def show_kernels(kernels, titles, cols, save, aspect = "own", show_nums = -1, sa
                                 ha="center", va="center", color="black")
         ax.set_title(titles[j*3+i])
         fig.colorbar(pcm, cax=cax)
-    if save_fig:
-        plt.savefig("{}.pdf".format(save), bbox_inches='tight')
+    if savefig:
+        plt.savefig(savefig, bbox_inches='tight')
     plt.show()
 
-def visualize_log_likelihood(log_liks, names, savefig, **kwargs):
+def visualize_mlls(mlls, names, savefig = None):
+    plt.figure(figsize = (10,6))
+    for idx, log_lik in enumerate(mlls):
+        lassos = log_lik.keys()
+        for l in lassos:
+            counter = 0
+            for mll in log_lik[l]:
+                if not counter:
+                    plt.plot(l, mll, color = COLORS[idx], label = names[idx], alpha = 0.8)
+                else:
+                    plt.plot(l, mll, color = COLORS[idx], alpha = 0.8)
+                counter += 1
+    
+    plt.grid(True)
+    plt.xlabel("Lasso coefficient")
+    plt.ylabel("MLL (train)")
+    plt.legend(prop = {'size': 14})
+    if savefig:
+        plt.savefig(savefig, bbox_inches='tight')
+    plt.show()
 
-    if not kwargs:
-        locals().update(DEFAULT_KWARGS)
-    else:
-        locals().update(kwargs)
 
-    plt.figure(figsize = figsize)
+def visualize_log_likelihood(log_liks, names, savefig = None):
+
+    plt.figure(figsize = (10,6))
     for idx, log_lik in enumerate(log_liks):
         lassos = log_lik.keys()
         counter = 0
@@ -136,12 +151,11 @@ def visualize_log_likelihood(log_liks, names, savefig, **kwargs):
                       plt.plot(l, ll, '.', color = COLORS[idx], markersize = 10)
                 else:
                     plt.plot(l, ll, '.', color = COLORS[idx], alpha = 0.2)
-    plt.ylim(ylim)
-    plt.xlim(xlim)
-    plt.grid(grid)
+
+    plt.grid(True)
     plt.xlabel("Lasso coefficient")
     plt.ylabel("log-likelihood (test)")
-    plt.legend(**legend)
+    plt.legend(prop = {'size': 14})
     if savefig:
-        plt.savefig("logliks.pdf")
+        plt.savefig(savefig, bbox_inches='tight')
     plt.show()
