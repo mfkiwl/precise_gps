9c28fe4823939a14cee31ed702ee1dbedc18f9c2
ylajaaski <juhoylajaaski@gmail.com>
2021-06-23 17:50:16 +0300

    fix : removed typo line

--- src/save_intermediate.py
+++ src/save_intermediate.py
@@ -3,12 +3,10 @@ from src.models.kernels import *
 import tensorflow as tf
 
 def save_results(model, step, params, counter, variances, likelihood_variances, mlls, l):
-    print(type(model).__name__, type(model).__name__ == 'SVILasso')
     if type(model).__name__ == 'SVILasso':
         value = model.maximum_log_likelihood_objective(model.train_data)
     else:
         value = model.maximum_log_likelihood_objective()
-    value = model.maximum_log_likelihood_objective()
     if step % 100 == 0:
         if type(model.kernel).__name__ == "FullGaussianKernel":
             L = model.kernel.L.numpy()
