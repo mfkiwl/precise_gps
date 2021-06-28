139ad25f069600acb77285c57503401a3cb93fbd
ylajaaski <juhoylajaaski@gmail.com>
2021-06-28 10:22:38 +0300

    refactor : new environment name

--- gpu_job.sh
+++ gpu_job.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-#SBATCH --gres=gpu:teslak80:1
-#SBATCH --time=12:00:00
+#SBATCH --gres=gpu:teslap100:1
+#SBATCH --time=24:00:00
 
 module load anaconda
 module load cuda
-source activate precise_gps
-
-cd documents/precise_gps
+source activate precise_gps_env
 
 chmod +x ./run_all.sh 
 srun --gres=gpu:1 ./run_all.sh "run_files"
