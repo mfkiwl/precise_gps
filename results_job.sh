40aa59a3e55f86cd57f2110168132b3276de243a
ylajaaski <juhoylajaaski@gmail.com>
2021-07-29 13:27:56 +0300

    refactor : automatic analysis for results

--- results_job.sh
+++ results_job.sh
@@ -3,12 +3,11 @@
 #SBATCH --time=6:00:00
 
 module load anaconda
+source activate precise_gps_env
 
 export TF_XLA_FLAGS=--tf_xla_enable_xla_devices
-srun --gres=gpu:1 python results.py -n "Boston" -d "boston" -l 4 -s 5
-srun --gres=gpu:1 python results.py -n "Redwine" -d "redwine" -l 4 -s 5
-srun --gres=gpu:1 python results.py -n "Concrete" -d "concrete" -l 4 -s 5
-srun --gres=gpu:1 python results.py -n "Energy" -d "energy" -l 4 -s 5
-srun --gres=gpu:1 python results.py -n "Whitewine" -d "whitewine" -l 4 -s 5
-srun --gres=gpu:1 python results.py -n "Protein" -d "protein" -l 4 -s 5
-srun --gres=gpu:1 python results.py -n "Yacht" -d "yacht" -l 4 -s 5
+results=$(ls results/raw)
+for result in $results
+do
+    srun --gres=gpu:1 python results.py -n "$result" -d "$result" -l 1 -s 5 - loss 1
+done
