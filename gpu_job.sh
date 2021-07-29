c7d70e1a145078ffdd5d0ac64f4bf855d5bb3e97
ylajaaski <juhoylajaaski@gmail.com>
2021-07-29 13:05:51 +0300

    refactor : changed gpu_job

--- gpu_job.sh
+++ gpu_job.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-#SBATCH --gres=gpu:1
-#SBATCH --time=24:00:00
 
-module load anaconda
-srun --gres=gpu:1 ./run_all.sh "run_files"
+file_name=$1
+> ${file_name}.sh 
+echo "#!/bin/bash" >> ${file_name}.sh
+echo "#SBATCH --gres=gpu:1" >> ${file_name}.sh
+echo "#SBATCH --time=12:00:00" >> ${file_name}.sh
+echo "#SBATCH --mem=24000M" >> ${file_name}.sh
+echo "#SBATCH --output=${file_name}.out" >> ${file_name}.sh
+
+echo "module load anaconda" >> ${file_name}.sh
+echo "source activate precise_gps_env" >> ${file_name}.sh
+echo "export TF_XLA_FLAGS=--tf_xla_enable_xla_devices" >> ${file_name}.sh
+echo "srun --gres=gpu:1 python train_models.py -f \"run_files/${file_name}.json\"" >> ${file_name}.sh
+
+sbatch ${file_name}.sh
